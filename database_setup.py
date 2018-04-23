d5d8aac456d1a688c5b6aa86c7f9ef02cb17ce75
keshibat <kensukeshibata@.com>
2018-04-23 15:39:12 +1000

    Add class User in database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -7,16 +7,35 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
 
 class Catalog(Base):
     __tablename__ = 'catalog'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
 
 
 class ClothingItem(Base):
-    __tablename__ = '_item'
+    __tablename__ = 'clothing_item'
 
     name = Column(String(80), nullable=False)
     id = Column(Integer, primary_key=True)
@@ -24,12 +43,14 @@ class ClothingItem(Base):
     price = Column(String(8))
     catalog_id = Column(Integer, ForeignKey('catalog.id'))
     catalog = relationship(Catalog)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
-# We added this serialize function to be able to send JSON objects in a
+# This serialize function to be able to send JSON objects in a
 # serializable format
     @property
     def serialize(self):
-
+        """Return object data in easily serializeable format"""
         return {
             'name': self.name,
             'description': self.description,
